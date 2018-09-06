a4abfc6a0e717bad6f3e4e49c8199d6bc3299a9d
lakshyajit165 <lakshyajit165@gmail.com>
2018-09-06 20:45:29 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+//HandleBars Middleware
+app.engine('handlebars', 
+	exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//index route
+app.get('/', (req,res)=>{
+	res.render('index');
+});
+
+//About Route
+app.get('/about', (req,res) =>{
+	res.render('about');
+});
+
+
+const port = 3000 || procecss.env.PORT;
+
+app.listen(port, () => {
+	console.log(`Server start on port ${port}`);
+});
