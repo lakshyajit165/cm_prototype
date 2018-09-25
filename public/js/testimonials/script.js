4af478d2d0e3d6545b4086527ec7fa6b0d438d26
lakshyajit165 <lakshyajit165@gmail.com>
2018-09-26 00:29:33 +0530

    Faculty Page and CMPT

---
+++ public/js/testimonials/script.js
@@ -0,0 +1,96 @@
+
+
+    
+    
+    
+    /*----------------------------------------------------*/
+    /*	Css3 Transition
+    /*----------------------------------------------------*/
+
+    
+    
+
+        
+
+
+
+////------- Testimonials Carousel
+	
+var testimonial = $("#testimonial-carousel");
+
+testimonial.owlCarousel({
+	navigation : true,
+	pagination: true,
+	slideSpeed : 2500,
+	stopOnHover: true,
+	autoPlay: 3000,
+	singleItem: true,
+	transitionStyle : "fade",
+	navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+});
+
+	
+
+
+
+
+
+
+
+
+
+// Team Carousel
+
+$(document).ready(function() {
+ 
+  $(".our-team").owlCarousel({
+        pagination: true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+        items: 4,
+    	//singleItem:true,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+
+////------- Partner/CLients
+
+$(document).ready(function() {
+ 
+  $(".clients").owlCarousel({
+        pagination: false,
+        navigation : true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+    	singleItem:false,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+        items: 5,
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+
+
+$ ( function () {
+
+	$(".video").fitVids();
+	
+});
+
+
+
+
+
+
